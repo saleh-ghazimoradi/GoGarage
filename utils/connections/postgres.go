03885d1f37e5b65378a67a110b5958b9320053ff
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-12 23:15:42 +0330

    implement post db connection

---
+++ utils/connections/postgres.go
@@ -0,0 +1,51 @@
+package connections
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+type PostgresConfig struct {
+	Host         string
+	Port         string
+	User         string
+	Password     string
+	Database     string
+	SSLMode      string
+	MaxOpenConns int
+	MaxIdleConns int
+	MaxIdleTime  time.Duration
+	Timeout      time.Duration
+}
+
+func PostgresURI(cfg PostgresConfig) string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
+func PostConnection(cfg PostgresConfig) (*sql.DB, error) {
+	connString := PostgresURI(cfg)
+
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to postgres: %w", err)
+	}
+
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	db.SetConnMaxLifetime(cfg.MaxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.Timeout)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("error pinging postgres: %w", err)
+	}
+
+	return db, nil
+}
+
+func PostgresUrl(cfg PostgresConfig) string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Database, cfg.SSLMode)
+}
